fb7b2468c9793430bbe17c8734f0d077496c3250
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2021-11-16 13:52:05 -0700

    Edited the drivetrain to turn based on controller position instead of joystick rotation

--- Test/src/main/java/frc/robot/DriveTrain.java
+++ Test/src/main/java/frc/robot/DriveTrain.java
@@ -1,54 +1,104 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj.Compressor;
 
 public class DriveTrain {
 
-    
     public static Spark leftMotor = new Spark(1);
     public static Spark rightMotor = new Spark(0);
-    public static XboxController cont = new XboxController(3);
+    public static Spark elevator = new Spark(2);
+    public static XboxController cont = new XboxController(0);
+    public static Solenoid beakOpen = new Solenoid(0);
+    public static Solenoid beakClose = new Solenoid(1);
+    public static Solenoid headFlatten = new Solenoid(2);
+    public static Solenoid headExtend = new Solenoid(3);
+    public static Compressor compressor = new Compressor(0);
+    public static Solenoid raiseFront = new Solenoid(4);
+    public static Solenoid raiseBack = new Solenoid(5);
+
 
     public static void drive() {
-        leftMotor.set(cont.getRawAxis(1));
-        rightMotor.set(cont.getRawAxis(3));
+
+        double speedModifierElevator = 0.25;
+        double speedModifierDriving = 0.3;
+        beakOpen.setPulseDuration(0.5);
+        beakClose.setPulseDuration(0.5);
+        headExtend.setPulseDuration(0.5);
+        headFlatten.setPulseDuration(0.5);
+        raiseFront.setPulseDuration(0.5);
+        raiseBack.setPulseDuration(0.5);
         
+
+        /*
+         * Button numbers: [1: X] [2: A] [3: B] [4: Y] [5: LB] [6: RB] [7: LT] [8: RT]
+         * [9: BACK] [10: START] [11: Left joystick] [12: Right joystick]
+         */
+
+        // Speed modifiers
+        if (cont.getRawButton(8)) {
+            speedModifierElevator = 0.5;
+            speedModifierDriving = 0.5;
+        } else if (cont.getRawButton(6)) {
+            speedModifierElevator = 0.2;
+            speedModifierDriving = 0.1;
+        } else {
+            speedModifierElevator = 0.25;
+            speedModifierDriving = 0.3;
+        }
+
+        // Set motors to controller position, using speed modifier
+        leftMotor.set(cont.getRawAxis(3) * speedModifierDriving);
+        rightMotor.set(-cont.getRawAxis(1) * speedModifierDriving);
+
+        // Elevator controls [Back Left buttons]
+        if (cont.getRawButton(5)) {
+            elevator.set(speedModifierElevator);
+        } else if (cont.getRawButton(7)) {
+            elevator.set(-speedModifierElevator);
+        } else {
+            elevator.set(0);
+        }
+
+        // Start compressor [START]
+        if (cont.getRawButtonPressed(10)) {
+            compressor.start();
+        }
+
+        // Stop compressor [BACK]
+        if (cont.getRawButtonPressed(9)) {
+            compressor.stop();
+        }
+
+        // Open beak [Button X]
+        if (cont.getRawButtonPressed(1)) {
+            beakOpen.startPulse();
+        }
+        // Close beak [Button A]
+        if (cont.getRawButtonPressed(2)) {
+            beakClose.startPulse();
+        }
+
+        // Flatten head [B]
+        if (cont.getRawButtonPressed(3)) {
+            headFlatten.startPulse();
+
+        }
+        // Extend head [Y]
+        if (cont.getRawButtonPressed(4)) {
+            headExtend.startPulse();
+        }
+
+        //Raise front [Right joystick]
+        if (cont.getRawButtonPressed(12)) {
+            raiseFront.startPulse();
+
+        //Raise front [Right joystick]
+        if (cont.getRawButtonPressed(11)) {
+            raiseBack.startPulse();
+        }
     }
 }
-
-//import java.awt.event.*;
-
-/*public static boolean F = false;
-public static boolean D = false;
-public static boolean L = false;
-public static boolean R = false;
-
-public void keyTyped(KeyEvent ke) {}
-public void keyReleased(KeyEvent ke) {}
-
-public void keyPressed(KeyEvent ke) {
-if (ke.getKeyCode() == KeyEvent.VK_UP) {
-    F = true;
 }
-else if (ke.getKeyCode() == KeyEvent.VK_DOWN) {
-    D = true;
-} 
-else if (ke.getKeyCode() == KeyEvent.VK_LEFT) {/*instruction for Left-key}
-else if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {/*instruction for Right-key}
-}
-
-
-
-public static void drive() {
-    if (F == true) {
-        leftMotor.set(1);
-        rightMotor.set(1);
-    }
-
-    if (D == true) {
-        leftMotor.set(-1);
-        rightMotor.set(-1);
-    }
-*/
