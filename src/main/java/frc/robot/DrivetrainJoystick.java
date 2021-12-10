2dd71c5820ff24da9fa3000906d5fbf2dd341167
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2021-12-09 17:22:42 -0700

    Deleted Excess Files

--- src/main/java/frc/robot/DrivetrainJoystick.java
+++ src/main/java/frc/robot/DrivetrainJoystick.java
@@ -13,8 +13,16 @@ public class DrivetrainJoystick {
     private static double left;
     private static double right;
     private static double speedModifierElevator = 0.25;
-    
-    // Set motors to controller position, using speed modifier
+    public static Spark leftMotor = new Spark(1);
+    public static Spark rightMotor = new Spark(0);
+    public static Spark elevator = new Spark(2);
+    public static Joystick cont = new Joystick(0);
+    public static Solenoid beakOpen = new Solenoid(0);
+    public static Solenoid beakClose = new Solenoid(1);
+    public static Solenoid headFlatten = new Solenoid(2);
+    public static Solenoid headExtend = new Solenoid(3);
+    public static Solenoid pistonsFront = new Solenoid(4);
+    public static Solenoid pistonsBack = new Solenoid(5);
     
     public static void drive() {
         
