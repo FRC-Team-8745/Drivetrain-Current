182e0d73709ee891125d9471d4cff9de85a7a9ba
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2021-12-15 21:25:48 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/FRC-Team-8745/drivetrain-new""
    This reverts commit d6ba05ef3fa2363e4df9d08e30c90a82427d8960.

---
+++ src/main/java/frc/robot/CentralComponents/Components.java
@@ -0,0 +1,26 @@
+package frc.robot.CentralComponents;
+
+// Declare imports for the Joystick, Solenoids, Sparks, and Compressor
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.XboxController;
+
+// Declare the components used for the robot (Joystick, Solenoids, Sparks, and
+// Compressor)
+public class Components {
+    public static Spark leftMotor = new Spark(1);
+    public static Spark rightMotor = new Spark(0);
+    public static Spark elevator = new Spark(2);
+    public static Joystick cont = new Joystick(0);
+    public static Solenoid beakOpen = new Solenoid(0);
+    public static Solenoid beakClose = new Solenoid(1);
+    public static Solenoid headFlatten = new Solenoid(2);
+    public static Solenoid headExtend = new Solenoid(3);
+    public static Solenoid pistonsFront = new Solenoid(4);
+    public static Solenoid pistonsBack = new Solenoid(5);
+    public static Compressor compressor = new Compressor();
+    public static XboxController Xbox = new XboxController(0);
+    public static Joystick contRC = new Joystick(0);
+}
