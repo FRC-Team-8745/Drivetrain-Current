46e2b516e3f6c3b711de1c220db9a88b82a9ecf7
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2021-12-16 18:26:46 -0700

    Revert "Added Gyro and CanBus stuff"
    This reverts commit fae681180bc9cdf074820ba74aeb5882c4ad17ec.

--- src/main/java/frc/robot/PIDtest.java
+++ src/main/java/frc/robot/PIDtest.java
@@ -2,6 +2,6 @@ package frc.robot;
 
 public class PIDtest {
     public static void test() {
-        PIDgyro.gyroDrive(0.1);
+        PIDgyro.gyroDrive(.1);
     }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -38,7 +38,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    Components.compressor.stop();
     DrivetrainJoystick.teleopInit();
     Components.gyro.calibrate();
     Components.gyro.reset();
