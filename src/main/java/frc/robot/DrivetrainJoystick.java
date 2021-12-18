4424443a964bab1ba3ae247edd7bd3c97a01d366
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2021-12-18 11:27:10 -0700

    Removed brackets; Moved init method to Components.

--- src/main/java/frc/robot/DrivetrainJoystick.java
+++ src/main/java/frc/robot/DrivetrainJoystick.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 //Import the central system for the components
 import frc.robot.CentralComponents.*;
 
@@ -11,27 +10,10 @@ import frc.robot.CentralComponents.*;
  */
 
 public class DrivetrainJoystick {
-    
-    // Declare variables for the two driving motors and set default speed mofifiers
-    private static double left;
-    private static double right;
-    private static double speedModifierDriving = 0.5;
-    private static double speedModifierElevator = 0.25;
 
-    // Method that runs when teleop is started
-    public static void teleopInit() {
-
-        // Sets the left motor to inverted
-        Components.leftMotor.setInverted(true);
-
-        // Defaults the compressor to off
-        Components.compressor.setClosedLoopControl(false);
-        Components.compressor.stop();
-
-        // Set double solenoid base values
-        Components.head.set(Value.kForward);
-        Components.beak.set(Value.kForward);
-    }
+    // Declare variables for the speed modifiers
+    private static double speedModifierDriving;
+    private static double speedModifierElevator;
 
     // Main drive method
     public static void drive() {
@@ -47,13 +29,12 @@ public class DrivetrainJoystick {
         }
 
         // Set variables for the left and right motors to the controllers axis, using
-        // both the up/down and left/right values and some math
-        right = -Components.cont.getRawAxis(1) * 0.5 + Components.cont.getRawAxis(0) * 0.5;
-        left = -Components.cont.getRawAxis(1) * 0.5 - Components.cont.getRawAxis(0) * 0.5;
-
-        // Set the motors to the speed determined above multiplied by the speed modifier
-        Components.leftMotor.set(left * speedModifierDriving);
-        Components.rightMotor.set(right * speedModifierDriving);
+        // both the up/down and left/right values and some math; multiplied by the speed
+        // modifier
+        Components.leftMotor.set(
+                (-Components.cont.getRawAxis(1) * 0.5 + Components.cont.getRawAxis(0) * 0.5) * speedModifierDriving);
+        Components.rightMotor.set(
+                (-Components.cont.getRawAxis(1) * 0.5 - Components.cont.getRawAxis(0) * 0.5) * speedModifierDriving);
 
         // Elevator controls [5+3]
         if (Components.cont.getRawButton(5))
@@ -67,32 +48,27 @@ public class DrivetrainJoystick {
             Components.elevator.set(0);
 
         // Front piston toggle [11]
-        if (Components.cont.getRawButtonPressed(11)) {
+        if (Components.cont.getRawButtonPressed(11))
             Components.pistonsFront.toggle();
-        }
 
         // Back piston toggle [12]
-        if (Components.cont.getRawButtonPressed(12)) {
+        if (Components.cont.getRawButtonPressed(12))
             Components.pistonsBack.toggle();
-        }
 
         // Toggle compressor [8]
         if (Components.cont.getRawButtonPressed(8)) {
-            if (Components.compressor.enabled()) 
+            if (Components.compressor.enabled())
                 Components.compressor.stop();
-             else 
+            else
                 Components.compressor.start();
-            
         }
 
         // Toggle beak [6]
-        if (Components.cont.getRawButtonPressed(6)) {
+        if (Components.cont.getRawButtonPressed(6))
             Components.beak.toggle();
-        }
 
         // Toggle head [4]
-        if (Components.cont.getRawButtonPressed(4)) {
+        if (Components.cont.getRawButtonPressed(4))
             Components.head.toggle();
-        }
     }
 }
