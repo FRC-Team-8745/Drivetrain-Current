8207c7a5a4b1c43da065869d9bf87b666f4fac9f
0riginRobot <94578836+0riginRobot@users.noreply.github.com>
2021-12-30 16:04:53 -0700

    Update Sweeper.java

--- src/main/java/frc/robot/Sweeper.java
+++ src/main/java/frc/robot/Sweeper.java
@@ -13,44 +13,46 @@ public class Sweeper {
 //name your motors, controller, and make the public static void drive here.
     public static Spark left = new Spark(0);
     public static Spark right = new Spark(1);
-    public static Spark grab = new Spark(2);
+    public static Spark grab = new Spark(8);
     public static XboxController driver = new XboxController(3);
     public static Compressor compressor = new Compressor();
     public static Solenoid fourBarOpen = new Solenoid(0);
     public static Solenoid fourBarClose = new Solenoid(1);
     public static void drive(){
 //set your speeds
-    double speed = 0.5;
-    double grabber = 0.2;
+    double speed = 0.4;
+    double grabber = 0.4;
     //drivetrain
         left.set(-driver.getRawAxis(1) *speed);
         right.set(driver.getRawAxis(3)*speed);
         //graber on Sweeper
-        if (driver.getRawButton(8)){
+        if (driver.getRawButton(8))
             grab.set(grabber);
-        } else if (driver.getRawButton(7)); {
+        else if (driver.getRawButton(7))
             grab.set(-grabber);
-        }  {
+        else
             grab.set(0);
-        }
+        
         //piston controls
         if (driver.getRawButtonPressed(2)){
-            if (compressorT) {
+            if (compressorT){
                 compressor.start();
                 compressorT = false;
-            } else if (!compressorT) {
+            }
+            else if (!compressorT){
                 compressor.stop();
                 compressorT = true;
             }
         }
             if (driver.getRawButtonPressed(3)) {
-                if (fourBarS) {
+                if (fourBarS){ 
                     fourBarClose.startPulse();
                     fourBarS = false;
-                } else if (!fourBarS) {
+                }
+                else if (!fourBarS) {
                     fourBarOpen.startPulse();
                     fourBarS = true;
                 }
-            }
         }
     }
+}
