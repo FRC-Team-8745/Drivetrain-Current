4049fce649d2696759f4f40eff20311d58be7900
Cooper <c112097@yahoo.com>
2022-01-06 16:35:17 -0700

    starter

---
+++ src/main/java/frc/robot/autonomous.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+//Import the central system for the components
+import frc.robot.CentralComponents.*;
+
+
+public class autonomous {
+    public static void drive() {
+        System.out.print("filler");
+    }
+}
