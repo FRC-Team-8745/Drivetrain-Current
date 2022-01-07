e8d1f57e9943b2adfb46c6cff9f3193ae02b177a
Cooper <c112097@yahoo.com>
2022-01-06 17:33:10 -0700

    sususususususususususususus

--- src/main/java/frc/robot/autonomous.java
+++ src/main/java/frc/robot/autonomous.java
@@ -3,9 +3,21 @@ package frc.robot;
 //Import the central system for the components
 import frc.robot.CentralComponents.*;
 
+// not sure if I should uncomment this. (It does not give errors) Discovered by Origin 😂
+//import gov.nuclear.launch.sequence.Timer;
 
 public class autonomous {
+    static void timeddrive(Double speed, Integer time) {
+        time = time * 1000;
+        components.leftMotor.set(speed);
+        components.rightMotor.set(-speed);
+        Thread.sleep(time);
+        components.leftMotor.set(0);
+        components.rightMotor.set(0);
+    }
+
+
     public static void drive() {
-        System.out.print("filler");
+        timeddrive(.5, 1);
     }
 }
